85d0cbaf5ffc14f06ea23cde0cc4186c56741ffb
mIO1000 <m.senda310@gmail.com>
2022-02-06 16:43:38 +0900

    初期登録

---
+++ scheduleSimul8_beta/src/main/java/com/scheduleSimul8/ScheduleSimul8BetaApplication.java
@@ -0,0 +1,13 @@
+package com.scheduleSimul8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleSimul8BetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleSimul8BetaApplication.class, args);
+	}
+
+}
---
+++ scheduleSimul8_beta/src/test/java/com/scheduleSimul8/ScheduleSimul8BetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleSimul8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleSimul8BetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
